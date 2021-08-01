5ec49323a968f23e4e834280400df9136343f4e5
korchie <korchie44@gmail.com>
2021-08-01 17:27:33 -0400

    added the last few test for getCoins()

--- tests/money-functions.test.js
+++ tests/money-functions.test.js
@@ -57,4 +57,16 @@ describe("getCoins", () => {
     expect(getCoins(32)).toStrictEqual({quarters: 1, dimes: 0, nickels: 1, pennies: 2});
   });
 
+  test("10 cents produces: quarters: 0, dimes: 1, nickels: 0, pennies: 0", () => {
+    expect(getCoins(10)).toStrictEqual({quarters: 0, dimes: 1, nickels: 0, pennies: 0});
+  });
+
+  test("27 cents produces: quarters: 1, dimes: 0, nickels: 0, pennies: 2", () => {
+    expect(getCoins(27)).toStrictEqual({quarters: 1, dimes: 0, nickels: 0, pennies: 2});
+  });
+
+  test("68 cents produces: quarters: 2, dimes: 1, nickels: 1, pennies: 3", () => {
+    expect(getCoins(68)).toStrictEqual({quarters: 2, dimes: 1, nickels: 1, pennies: 3});
+  });
+
 });
